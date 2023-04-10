67f2bd779333849bce671b01d0d5da1a4bafd708
Joey <joeyrjtd@gmail.com>
2023-04-10 16:26:20 -0300

    changes

--- react_django/settings.py
+++ react_django/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'reactapp',
 ]
 
 ROOT_URLCONF = 'react_django.urls'
