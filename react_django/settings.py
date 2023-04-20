2dfa154cc3d130433f88dc8e279bf123557d5ce0
Joey <joeyrjtd@gmail.com>
2023-04-20 11:22:01 -0300

    changes

--- react_django/settings.py
+++ react_django/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'reactapp/build'),
+            os.path.join(BASE_DIR, 'reactapp/public'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
